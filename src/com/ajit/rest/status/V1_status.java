c5d2476e3c400f3187d563700ed35ebdc7a11bac
ajit <ajit.hegde91@gmail.com>
2016-01-21 02:27:23 -0500

    change in V1

--- src/com/ajit/rest/status/V1_status.java
+++ src/com/ajit/rest/status/V1_status.java
@@ -4,9 +4,19 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 @Path("/v1/status")
 public class V1_status {
+	private static final String api_ver = "00.00";
+	
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String returnTitle(){
 		return "<p>Java Web Service</p>";
 	}
+	
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion(){
+		return "<strong>Version:</strong>"+api_ver;
+	}
+	
 }
